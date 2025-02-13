383dd79bb7bef1f8bf9cec45a5a7524338f31431
japajapajap <fazaibadurrohman@gmail.com>
2025-02-14 00:38:36 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Serius?",
-    "Ntar nyesel lho??",
+    "Are you sure?",
+    "Really sure??",
     "Are you positive?",
-    "Please please please please...",
+    "Pookie please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! "
+    "Just kidding, say yes please! ❤️"
 ];
 
 let messageIndex = 0;
